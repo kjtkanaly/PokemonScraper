83de3ef3ce33a8c7821a01acd0e2b311b780ac45
kjtkanaly <kjtkanaly@gmail.com>
2023-09-10 11:40:04 -0400

    Working on expanding the functionality.

---
+++ TcgPKMN.py
@@ -0,0 +1,41 @@
+import csv
+from pokemontcgsdk import Card
+
+def GetCardsInSet(setID):
+
+    cards = Card.where(q=("set.id:" + setID))
+    infoList = []
+
+    for card in cards:
+        cardPrices = card.tcgplayer.prices
+
+        cardInfo = [card.id, card.number, card.name, card.rarity, card.artist, 
+                    CheckIfPokeCardHasPrice(cardPrices.normal), 
+                    CheckIfPokeCardHasPrice(cardPrices.holofoil),
+                    CheckIfPokeCardHasPrice(cardPrices.reverseHolofoil), 
+                    CheckIfPokeCardHasPrice(cardPrices.firstEditionHolofoil),
+                    CheckIfPokeCardHasPrice(cardPrices.firstEditionNormal),
+                    card.tcgplayer.updatedAt]
+        
+        infoList.append(cardInfo)
+    
+    return infoList
+
+
+def CheckIfPokeCardHasPrice(cardPrice):
+    if not(cardPrice == None):
+        return cardPrice.market
+    else:
+        return None
+    
+def InitPokeCSV(fileName, column):
+    with open(fileName, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(column)
+
+
+def AppendPokeCSV(fileName, infoList):
+    with open(fileName, 'a', newline='') as file:
+        writer = csv.writer(file)
+        for info in infoList:
+            writer.writerow(info)
--- main.py
+++ main.py
@@ -1,67 +1,24 @@
-import csv
-import numpy as np
-from pokemontcgsdk import Card
 from pokemontcgsdk import RestClient
+import TcgPKMN as TP
 import config
 
-
-def getCardsInSet(setID, fileName):
-
-    cards = Card.where(q=("set.id:" + setID))
-
-    for card in cards:
-        cardPrices = card.tcgplayer.prices
-
-        cardInfo = [card.id, card.number, card.name, card.rarity, card.artist, 
-                    checkIfPokeCardHasPrice(cardPrices.normal), 
-                    checkIfPokeCardHasPrice(cardPrices.holofoil),
-                    checkIfPokeCardHasPrice(cardPrices.reverseHolofoil), 
-                    checkIfPokeCardHasPrice(cardPrices.firstEditionHolofoil),
-                    checkIfPokeCardHasPrice(cardPrices.firstEditionNormal),
-                    card.tcgplayer.updatedAt]
-
-        appendPokeCSV(fileName, cardInfo)
-
-
-def checkIfPokeCardHasPrice(cardPrice):
-    if not(cardPrice == None):
-        return cardPrice.market
-    else:
-        return None
-
-
-def initPokeCSV(fileName, column):
-    with open(fileName, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(column)
-
-
-def appendPokeCSV(fileName, cardInfo):
-    with open(fileName, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(cardInfo)
-
-
-def main():
-
-    stdCSVFile = "CrownZenith - STD Set.csv"
-    ggCSVFile = "CrownZenith - GG Set.csv"
+def SetupSetCSV(setID, setCSVFile):
     csvColumn = ["ID", "Set Number", "Card Name", "Rarity", "Artist",
                  "Normal Price($)", "Holo Foil Price ($)", "Reverse Holo Price ($)", 
                  "First Edition Holo Price ($)", "First Edition Normal Price ($)",
                  "Updated At (YYYY/MM/DD)"]
 
-    initPokeCSV(stdCSVFile, csvColumn)
-    initPokeCSV(ggCSVFile, csvColumn)
+    TP.InitPokeCSV(setCSVFile, csvColumn)
+    infoList = TP.GetCardsInSet(setID)
+    TP.AppendPokeCSV(infoList)
 
+
+def main():
     pokeAPIkey = config.getPokeAPIkey()
     RestClient.configure(pokeAPIkey)
 
+    stdCSVFile = "CrownZenith - STD Set.csv"
     stdId = "swsh12pt5"
-    ggId = "swsh12pt5gg"
-
-    getCardsInSet(stdId, stdCSVFile)
-    getCardsInSet(ggId, ggCSVFile)
 
 
 if __name__ == "__main__":
